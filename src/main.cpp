f8fe2df6f0f7d3180db5c1b92726be2e87fa1114
elywacime <yousfi.wacime@gmail.com>
2025-07-30 18:32:52 +0000

    second

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "../include/LoopDeLoop.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+    return 1;
+  }
+
+  int port = std::atoi(argv[1]);
+  std::string password = argv[2];
+
+  try {
+    LoopDeLoop server(port, password);
+    server.run(); // Start event loop
+  } catch (const std::exception &e) {
+    std::cerr << "Fatal error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
